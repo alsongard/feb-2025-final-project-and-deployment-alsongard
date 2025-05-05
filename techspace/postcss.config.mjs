ede910946ac660852a74140c11cdc50cfda0a8cd
alsongard <alsongard@gmail.com>
2025-05-05 23:20:59 +0300

    techspace done

--- techspace/postcss.config.mjs
+++ techspace/postcss.config.mjs
@@ -1,13 +1,5 @@
 const config = {
   plugins: ["@tailwindcss/postcss"],
-  eslint: {
-    // Dangerously allow production builds to successfully complete even if your project has eslint errors.
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    // Dangerously allow production builds to successfully complete even if your project has type errors.
-    ignoreBuildErrors: true,
-  }
 };
 
 export default config;
