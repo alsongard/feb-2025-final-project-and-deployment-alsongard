fb624168a0f792516559292d017a6aadc0b44526
alsongard <alsongard@gmail.com>
2025-05-05 14:20:29 +0300

    homePage data added

---
+++ techspace/app/lib/data.ts
@@ -0,0 +1,27 @@
+export const techInfoData = [
+    {
+        id:1,
+        img:"/image/backiee-1315-landscape.jpg",
+        imgTxt: "Webb's First Deep Field"
+    },
+    {
+        id:3,
+        img:"/image/web_spectra.png",
+        imgTxt: "Webb's First Deep Field"
+    },
+    {
+        id:4,
+        img:"/image/planetary_engulfment.png",
+        imgTxt: "Webb's First Deep Field"
+    },
+    {
+        id:5,
+        img:"/image/backiee-1315-landscape.jpg",
+        imgTxt: "Webb's First Deep Field"
+    },
+    {
+        id:6,
+        imgTxt: "Webb's First Deep Field",
+        img:"/image/Herbig-Haro.jpg"
+    }
+]
