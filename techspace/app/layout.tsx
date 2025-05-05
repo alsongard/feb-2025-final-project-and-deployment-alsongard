66c42078b41de5aabf33eb18ae189c244a7e8c0a
alsongard <alsongard@gmail.com>
2025-05-05 20:07:10 +0300

    layout added styles

--- techspace/app/layout.tsx
+++ techspace/app/layout.tsx
@@ -1,6 +1,15 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Footer from "./components/footer";
+import Header from "./components/header";
+import {Orbitron} from 'next/font/google'
+const orbitron = Orbitron(
+    { 
+        subsets: ['latin'],
+        weight: ['400', '500','600', '700']
+    }
+)
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,9 +34,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${orbitron.className} antialiased`}
       >
+        <Header/>
         {children}
+        <Footer/>
       </body>
     </html>
   );
