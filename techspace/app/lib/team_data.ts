6aab8258eacf59288a956274f6e0b130974514b0
alsongard <alsongard@gmail.com>
2025-05-05 14:20:47 +0300

    aboutpage data added

---
+++ techspace/app/lib/team_data.ts
@@ -0,0 +1,51 @@
+export const teamData = [
+    {
+        id:8,
+        img: "/image/backiee-96330-landscape.jpg",
+        alt: "Einstein Liskov",
+        name: "Einstein Liskov",
+        topics:"Nuclear Physics & Space Relativity"
+    },
+    {
+        id:2,
+        img: "/image/backiee-96330-landscape.jpg",
+        alt: "Einstein Liskov",
+        name: "Einstein Liskov",
+        topics:"Nuclear Physics & Space Relativity"
+    },
+    {
+        id:3,
+        img: "/image/backiee-96330-landscape.jpg",
+        alt: "Einstein Liskov",
+        name: "Einstein Liskov",
+        topics:"Nuclear Physics & Space Relativity"
+    },
+    {
+        id:4,
+        img: "/image/backiee-96330-landscape.jpg",
+        alt: "Einstein Liskov",
+        name: "Einstein Liskov",
+        topics:"Nuclear Physics & Space Relativity"
+    },
+    {
+        id:5,
+        img: "/image/backiee-96330-landscape.jpg",
+        alt: "Einstein Liskov",
+        name: "Einstein Liskov",
+        topics:"Nuclear Physics & Space Relativity"
+    },
+    {
+        id:6,
+        img: "/image/backiee-96330-landscape.jpg",
+        alt: "Einstein Liskov",
+        name: "Einstein Liskov",
+        topics:"Nuclear Physics & Space Relativity"
+    },
+    {
+        id:7,
+        img: "/image/backiee-96330-landscape.jpg",
+        alt: "Einstein Liskov",
+        name: "Einstein Liskov",
+        topics:"Nuclear Physics & Space Relativity"
+    },
+]
