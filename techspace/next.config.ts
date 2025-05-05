5f25728f79b860bfbfb29fbc7629440f244ea97b
alsongard <alsongard@gmail.com>
2025-05-05 23:19:40 +0300

    techspace added

--- techspace/next.config.ts
+++ techspace/next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    // Dangerously allow production builds to successfully complete even if your project has eslint errors.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // Dangerously allow production builds to successfully complete even if your project has type errors.
+    ignoreBuildErrors: true,
+  }
 };
 
 export default nextConfig;
