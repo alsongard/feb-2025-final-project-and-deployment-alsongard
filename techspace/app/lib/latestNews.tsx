f201e98be0e2bffc46c4853b75cd3e4c7118d40e
alsongard <alsongard@gmail.com>
2025-05-05 20:06:03 +0300

    latest data added

---
+++ techspace/app/lib/latestNews.tsx
@@ -0,0 +1,115 @@
+export const latestData = {
+    'Technology': [
+        {
+            id:1,
+            img:"/image/backiee-68622-landscape.jpg",
+            altText: "car_image",
+            latestNews: "New BMW",
+            externalLink: "https://google.com"
+        },
+        {
+            id:2,
+            img:"/image/backiee-95715.jpg",
+            altText: "car_image",
+            latestNews: "New BMW",
+            externalLink: "https://google.com"
+        },
+        {
+            id:3,
+            img:"/image/backiee-86829-landscape.jpg",
+            altText: "car_image",
+            latestNews: "New BMW",
+            externalLink: "https://google.com"
+        },
+        {
+            id:4,
+            img:"/image/backiee-20410-landscape.jpg",
+            altText: "car_image",
+            latestNews: "New BMW",
+            externalLink: "https://google.com"
+        },
+        {
+            id:5,
+            img:"/image/backiee-76410-landscape.jpg",
+            altText: "car_image",
+            latestNews: "New BMW",
+            externalLink: "https://google.com"
+        }
+    ],
+    'Space':[
+        {
+            id:1,
+            img:"/image/backiee-19212-landscape.jpg",
+            altText: "car_image",
+            latestNews: "New BMW",
+            externalLink: "https://google.com"
+        },
+        {
+            id:2,
+            img:"/image/backiee-105012-landscape.jpg",
+            altText: "car_image",
+            latestNews: "New BMW",
+            externalLink: "https://google.com"
+        },
+        {
+            id:3,
+            img:"/image/backiee-115417-landscape.jpg",
+            altText: "car_image",
+            latestNews: "New BMW",
+            externalLink: "https://google.com"
+        },
+        {
+            id:4,
+            img:"/image/backiee-138210-landscape.jpg",
+            altText: "car_image",
+            latestNews: "New BMW",
+            externalLink: "https://google.com"
+        },
+        {
+            id:5,
+            img:"/image/backiee-88224-landscape.jpg",
+            altText: "car_image",
+            latestNews: "New BMW",
+            externalLink: "https://google.com"
+        }        
+    ],
+    'Science':
+    [
+        {
+            id:1,
+            img:"/image/backiee-103936-landscape.jpg",
+            altText: "car_image",
+            latestNews: "New BMW",
+            externalLink: "https://google.com"
+        },
+        {
+            id:2,
+            img:"/image/backiee-183802-landscape.jpg",
+            altText: "car_image",
+            latestNews: "New BMW",
+            externalLink: "https://google.com"
+        },
+        {
+            id:3,
+            img:"/image/backiee-183818-landscape.jpg",
+            altText: "car_image",
+            latestNews: "New BMW",
+            externalLink: "https://google.com"
+        },
+        {
+            id:4,
+            img:"/image/backiee-183818-landscape.jpg",
+            altText: "car_image",
+            latestNews: "New BMW",
+            externalLink: "https://google.com"
+        },
+        {
+            id:5,
+            img:"/image/backiee-183818-landscape.jpg",
+            altText: "car_image",
+            latestNews: "New BMW",
+            externalLink: "https://google.com"
+        }
+        
+    ]
+}
